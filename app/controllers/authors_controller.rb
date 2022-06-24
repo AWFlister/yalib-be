860eb79323605b1446ea4bb74ecea92789dda77a
Aryasakti Wirasena <aryasaktiwirasena@gmail.com>
2022-06-24 14:27:14 +0700

    Init author CRUD and index method

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,20 @@
+class AuthorsController < ApplicationController
+  def index
+    @authors = Author.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :authors, except: ['show']
   root 'pages#home'
 end
